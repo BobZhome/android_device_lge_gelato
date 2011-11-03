2cbcb01a5fe8c8b2563d501493dad83e4cd062e2
BobZhome <BobZhome@Cheerful.com>
2011-11-03 17:46:51 -0400

    fixs to get cm to build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,60 +1,76 @@
-USE_CAMERA_STUB := false
-BOARD_USE_FROYO_LIBCAMERA := true
-
+# CPU
 TARGET_BOARD_PLATFORM := msm7k
-TARGET_ARCH_VARIANT := armv6-vfp
-TARGET_CPU_ABI := armeabi-v6l
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_CPU_ABI := armeabi
 TARGET_CPU_ABI2 := armeabi
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Target properties
 TARGET_BOOTLOADER_BOARD_NAME := thunderc
+TARGET_OTA_ASSERT_DEVICE := VM701,thunderc,VM701
 
-TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
-BOARD_NO_RGBX_8888 := true
-BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
-TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
-
-TARGET_PROVIDES_INIT_TARGET_RC := true
-
-TARGET_USES_OLD_LIBSENSORS_HAL := true
-
-TARGET_OTA_ASSERT_DEVICE := thunderc
-
+# Target information
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
 TARGET_NO_RADIOIMAGE := true
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# QCOM
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_LIBS := true
 BOARD_USES_QCOM_LIBRPC := true
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_FORCE_STATIC_A2DP := true
 
-# Using GPSSHIM so that we can use the LG/Qualcomm binary blobs because
-# the open source stuff seems to be buggy, and Qualcomm doesn't believe
-# in open source.
+# GPS
 BOARD_USES_GPSSHIM := true
 BOARD_GPS_NEEDS_XTRA := true
 BOARD_GPS_LIBRARIES := libloc
 
-BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-BOARD_CUSTOM_USB_CONTROLLER := ../../device/lge/slider/netd/UsbController.cpp
+# Browser
+JS_ENGINE := v8
 
-# VER_0_6_X does not search networks
-WPA_SUPPLICANT_VERSION := VER_0_5_X
+# USB mass storage
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/lge/thunderc/netd/UsbController.cpp
+
+# Sensors
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+
+# Audio
+#BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := true
+BUILD_WITH_FULL_STAGEFRIGHT := true
+#TARGET_PROVIDES_LIBAUDIO := true 
+
+# Graphics
+TARGET_HARDWARE_3D := false
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_SF_NEEDS_REAL_DIMENSIONS := true
+BOARD_NO_RGBX_8888 := true
+BOARD_EGL_CFG := device/lge/thunderc/files/lib/egl/egl.cfg
+BOARD_HAS_LIMITED_EGL := true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
+
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+
+# WiFI
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-BOARD_WLAN_DEVICE := bcm4325
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wireless.ko"
+BOARD_WLAN_DEVICE := bcm4330
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wl/rtecdc.bin nvram_path=/system/etc/wl/nvram.txt"
-WIFI_DRIVER_MODULE_NAME := wireless
-## Tethering is not working now
+WIFI_DRIVER_MODULE_NAME := "wireless"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wireless.ko"
 WIFI_DRIVER_FW_STA_PATH := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_FW_AP_PATH := "/system/etc/wl/rtecdc-apsta.bin"
 WIFI_DRIVER_HAS_LGE_SOFTAP := true
+WPA_SUPPLICANT_VERSION := VER_0_6_X
 
-BOARD_EGL_CFG := device/lge/thunderc/files/lib/egl/egl.cfg
-
-BOARD_KERNEL_CMDLINE := mem=477M console=ttyMSM2,115200n8 androidboot.hardware=thunderc
+# Kernel
 BOARD_KERNEL_BASE := 0x12200000
+BOARD_KERNEL_CMDLINE := mem=477M console=ttyMSM2,115200n8 androidboot.hardware=thunderc
 BOARD_PAGE_SIZE := 0x00000800
 
 # Appropriate defaults?
@@ -82,12 +98,11 @@ endif
 WITH_DEXPREOPT := false
 WITH_JIT := true
 ENABLE_JSC_JIT := true
-JS_ENGINE := v8
 
-BUILD_WITH_FULL_STAGEFRIGHT := true
-TARGET_PROVIDES_LIBAUDIO := true 
-# TARGET_PROVIDES_LIBRIL = vendor/lge/slider/proprietary/system/lib/libril-qc-1.so
+# TARGET_PROVIDES_LIBRIL = vendor/lge/thunderc/proprietary/system/lib/libril-qc-1.so
 
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+# FM Radio
+#BOARD_HAVE_FM_RADIO := false
+#BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
 TARGET_SF_NEEDS_REAL_DIMENSIONS := true
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cyanogen_slider_VM701-eng
-add_lunch_combo cyanogen_slider_VS700-eng
+add_lunch_combo cyanogen_thunderc_VM701-eng
+add_lunch_combo cyanogen_thunderc_VS700-eng
